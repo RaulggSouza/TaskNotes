746f01291395cf8fc36da8049ec91ab092e4c57a
RaulggSouza <raulggsouza@hotmail.com>
2026-06-13 17:26:38 -0300

    feat: create app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return {
+      message: 'Task Manager NestJS API'
+    }
+  }
+
+  @Get('health')
+  health() {
+    return {
+      status: 'ok',
+      message: 'API is running'
+    }
   }
 }
