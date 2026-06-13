ec3be103de0a49926d77749255002c9fc3286bd2
RaulggSouza <raulggsouza@hotmail.com>
2026-06-13 18:17:32 -0300

    feat: create docker-compose

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { Task } from './task.entity'
 import { TasksController } from './tasks.controller'
 import { TasksService } from './tasks.service'
+import { RedisModule } from '../redis/redis.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task]), RedisModule],
   controllers: [TasksController],
   providers: [TasksService]
 })
