87d244b7ab1d526c1ee74b14f7164e76dabf192a
RaulggSouza <raulggsouza@hotmail.com>
2026-06-13 17:25:55 -0300

    refactor: extract redis folder to src

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DATABASE_HOST'),
+        port: Number(config.get<string>('DATABASE_PORT')),
+        username: config.get<string>('DATABASE_USER'),
+        password: config.get<string>('DATABASE_PASSWORD'),
+        database: config.get<string>('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+        retryAttempts: 10,
+        retryDelay: 3000,
+      }),
+    }),
+
+    RedisModule,
+    TasksModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,84 +1,84 @@
-import { Injectable, NotFoundException } from '@nestjs/common'
-import { InjectRepository } from '@nestjs/typeorm'
-import { Repository } from 'typeorm'
-import { CreateTaskDto } from './dto/create-task.dto'
-import { UpdateTaskDto } from './dto/update-task.dto'
-import { Task } from './task.entity'
-import { RedisService } from './redis/redis.service'
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { Task } from './task.entity';
+import { RedisService } from '../redis/redis.service';
 
 @Injectable()
 export class TasksService {
-  private readonly tasksCacheKey = 'tasks:all'
+  private readonly tasksCacheKey = 'tasks:all';
 
   constructor(
     @InjectRepository(Task)
     private readonly taskRepository: Repository<Task>,
-    private readonly redisService: RedisService
+    private readonly redisService: RedisService,
   ) {}
 
   async create(createTaskDto: CreateTaskDto): Promise<Task> {
-    const task = this.taskRepository.create(createTaskDto)
+    const task = this.taskRepository.create(createTaskDto);
 
-    const savedTask = await this.taskRepository.save(task)
+    const savedTask = await this.taskRepository.save(task);
 
-    await this.clearTasksCache()
+    await this.clearTasksCache();
 
-    return savedTask
+    return savedTask;
   }
 
   async findAll(): Promise<Task[]> {
-    const cachedTasks = await this.redisService.get(this.tasksCacheKey)
+    const cachedTasks = await this.redisService.get(this.tasksCacheKey);
 
     if (cachedTasks) {
-      return JSON.parse(cachedTasks)
+      return JSON.parse(cachedTasks);
     }
 
     const tasks = await this.taskRepository.find({
       order: {
-        id: 'ASC'
-      }
-    })
+        id: 'ASC',
+      },
+    });
 
-    await this.redisService.set(this.tasksCacheKey, JSON.stringify(tasks), 60)
+    await this.redisService.set(this.tasksCacheKey, JSON.stringify(tasks), 60);
 
-    return tasks
+    return tasks;
   }
 
   async findOne(id: number): Promise<Task> {
     const task = await this.taskRepository.findOne({
       where: {
-        id
-      }
-    })
+        id,
+      },
+    });
 
     if (!task) {
-      throw new NotFoundException(`Task with id ${id} not found`)
+      throw new NotFoundException(`Task with id ${id} not found`);
     }
 
-    return task
+    return task;
   }
 
   async update(id: number, updateTaskDto: UpdateTaskDto): Promise<Task> {
-    const task = await this.findOne(id)
+    const task = await this.findOne(id);
 
-    Object.assign(task, updateTaskDto)
+    Object.assign(task, updateTaskDto);
 
-    const updatedTask = await this.taskRepository.save(task)
+    const updatedTask = await this.taskRepository.save(task);
 
-    await this.clearTasksCache()
+    await this.clearTasksCache();
 
-    return updatedTask
+    return updatedTask;
   }
 
   async remove(id: number): Promise<void> {
-    const task = await this.findOne(id)
+    const task = await this.findOne(id);
 
-    await this.taskRepository.remove(task)
+    await this.taskRepository.remove(task);
 
-    await this.clearTasksCache()
+    await this.clearTasksCache();
   }
 
   private async clearTasksCache(): Promise<void> {
-    await this.redisService.del(this.tasksCacheKey)
+    await this.redisService.del(this.tasksCacheKey);
   }
 }
